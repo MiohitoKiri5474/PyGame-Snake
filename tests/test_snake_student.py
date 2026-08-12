fc4d3d3002b4a86e7d7d312d31f7effb2ffe1861
MiohitoKiri5474 <lltzpp@gmail.com>
2026-08-12 16:24:52 +0800

    test: full test for hit_wall

--- tests/test_snake_student.py
+++ tests/test_snake_student.py
@@ -28,3 +28,15 @@ def test_range_of_head_positions() -> None:
 def test_ate_food() -> None:
     assert ate_food((20, 40), (20, 40)) is True
     assert ate_food((60, 20), (20, 20)) is False
+
+
+def test_hit_wall() -> None:
+    assert hit_wall((0, 0), 640, 480, 20) is False
+    assert hit_wall((100, 100), 640, 480, 20) is False
+    assert hit_wall((620, 0), 640, 480, 20) is False
+    assert hit_wall((0, 460), 640, 480, 20) is False
+
+    assert hit_wall((640, 100), 640, 480, 20) is True
+    assert hit_wall((100, 480), 640, 480, 20) is True
+    assert hit_wall((-20, 100), 640, 480, 20) is True
+    assert hit_wall((100, -20), 640, 480, 20) is True
